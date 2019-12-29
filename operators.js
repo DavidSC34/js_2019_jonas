7777507889e19d0a782fe926802232ff17d7d136
DavidSC34 <david@wbcboxing.com>
2019-12-28 23:32:05 -0600

    Js , operadores basicos, primer commit del curso

---
+++ operators.js
@@ -0,0 +1,32 @@
+var year , yearJohn, yearMark;
+
+now = 2018;
+agejohn= 28;
+ageMark = 33;
+
+//Math operators
+yearJohn = now - agejohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+
+
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+//logical operators
+
+var johnOlder = agejohn < ageMark;
+console.log(johnOlder);
+
+//Type of operator
+
+console.log(typeof johnOlder);
+console.log(typeof agejohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
